d03ff5e1d636e97ec6e0cb6d2a6a11eec15a85c4
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2022-07-22 00:16:37 -0400

    Completed Spotify authentication. Playlists can now be accessed and displayed

--- routes/playlists.js
+++ routes/playlists.js
@@ -8,7 +8,8 @@ const qs = require("querystring");
 const Router = express.Router();
 
 // Urls
-const API_URL = "https://youtube.googleapis.com/youtube/v3/playlists";
+const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3/playlists";
+const SPOTIFY_API_URL = "https://api.spotify.com/v1"; 
 
 // Playlist data returned from requests to YouTube and Spotify APIs
 var youtubePlaylistData, spotifyPlaylistData;
@@ -18,7 +19,10 @@ Router.get("/", [retrieveYouTubePlaylists, retrieveSpotifyPlaylists], (req,res) 
 	// let playlistsData = await retrievePlaylists();
 	const compiledPug = pug.compileFile("./views/playlists.pug");
 
-	res.send(compiledPug({ youtubePlaylistData: youtubePlaylistData.data.items }));
+	res.send(compiledPug({ 
+		youtubePlaylistData: youtubePlaylistData.items, 
+		spotifyPlaylistData: spotifyPlaylistData.items
+	}));
 });
 
 async function retrieveYouTubePlaylists(req,res,next) {
@@ -29,18 +33,28 @@ async function retrieveYouTubePlaylists(req,res,next) {
 	}
 	let formattedParams = qs.stringify(params);
 
-	youtubePlaylistData = await axios.get(`${API_URL}?${formattedParams}`,{
+	let youtubeResponse = await axios.get(`${YOUTUBE_API_URL}?${formattedParams}`,{
 		headers: {
 			"Authorization": `Bearer ${req.cookies["youtube-access-token"]}`,
 			"Accept": "application/json"
 		}
 	});
+	// Retrieve the playlist data from the response
+	youtubePlaylistData = youtubeResponse.data;
 
 	// Go to next middleware
 	next();
 }
-function retrieveSpotifyPlaylists(req,res,next) {
-	console.log("Spotify middleware for retrieving playlists");
+async function retrieveSpotifyPlaylists(req,res,next) {
+	let spotifyResponse = await axios.get(`${SPOTIFY_API_URL}/me/playlists`,{
+		headers: {
+			"Authorization": `Bearer ${req.cookies["spotify-access-token"]}`,
+			"Content-Type": "application/json"
+		}
+	});
+	// Retrieve the playlist data from the response
+	spotifyPlaylistData = spotifyResponse.data;
+
 	// Go to next middleware
 	next();
 }
--- routes/spotify.js
+++ routes/spotify.js
@@ -3,17 +3,72 @@ const axios = require("axios");
 // Include querystring to allow for easy parameter creation
 const qs = require("querystring");
 
-// Urls
-const REDIRECT_URI = "http://localhost:8080/spotify/authorize";
-
 // Initialize Express Router
 const Router = express.Router();
 
+// Urls
+const AUTHORIZATION_ENDPOINT = "https://accounts.spotify.com/authorize";
+const TOKEN_SERVER = "https://accounts.spotify.com/api/token";
+const REDIRECT_URI = "http://localhost:8080/spotify/authorize";
+const SCOPES = "playlist-modify-public"
+
+// State - random string to prevent any man in the middle attacks
+const STATE = "cjPekA39=dnjkl3SK-d3fdsa3GA$";
+
+let accessToken, code;
+
 Router.get("/login", (req,res) => {
-	res.redirect("/my-playlists");
+	if (accessToken === undefined)
+		startAuthorizing(res);
 });
 Router.get("/authorize", (req,res) => {
-	res.end();
+	if (req.query.code)
+		if (req.query.state === STATE)
+			code = req.query.code;
+	if (!accessToken && !code)
+		startAuthorizing(res);
+	else
+		getAccessToken(res);
 });
+
+function startAuthorizing(res) {
+	let params = {
+		client_id: process.env.SPOTIFY_CLIENT_ID,
+		response_type: "code",
+		redirect_uri: REDIRECT_URI,
+		state: STATE,
+		scope: SCOPES
+	}
+	let formattedParams = qs.stringify(params);
+
+	res.redirect(`${AUTHORIZATION_ENDPOINT}?${formattedParams}`);
+}
+function getAccessToken(res) {
+	let params = {
+		grant_type: 'authorization_code',
+		code,
+		redirect_uri: REDIRECT_URI
+	}
+	let formattedParams = qs.stringify(params);
+
+	axios.post(TOKEN_SERVER,formattedParams,{
+		headers: {
+			"Authorization": "Basic " + (Buffer.from(process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET).toString("base64")),
+			"Content-Type": "application/x-www-form-urlencoded"
+		}
+	}).then((response) => {
+		// Set access token locally for any possible future access
+		accessToken = response.data.access_token;
+		// Store in cookie to be accessed after redirection
+		res.cookie("spotify-access-token",accessToken);
+		// Reset code
+		code = null;
+		// Redirect to playlists
+		res.redirect("/my-playlists");
+	}).catch((err) => {
+		console.log(err);
+		res.end();
+	})
+}
 
 module.exports = Router;
